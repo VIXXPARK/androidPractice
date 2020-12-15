23f095c5a5625df31d06c1df84c6a6d69ba63932
VIXXPARK <vixx170627@gmail.com>
2020-12-15 17:43:17 +0900

    setColorFilter change to BlendModeColorFilter

--- app/src/main/java/com/example/burtbees/navigation/UserFragment.kt
+++ app/src/main/java/com/example/burtbees/navigation/UserFragment.kt
@@ -2,6 +2,8 @@ package com.example.burtbees.navigation
 
 import android.content.Intent
 import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.os.Build
@@ -95,10 +97,7 @@ class UserFragment:Fragment(){
                 fragmentView?.findViewById<TextView>(R.id.account_tv_follwer_count)?.text = followDTO?.followerCount?.toString()
                 if (followDTO?.follwers?.containsKey(currentUserUid!!)){
                     fragmentView?.findViewById<Button>(R.id.account_btn_follow_signout)?.text = getString(R.string.follow_cancel)
-                    fragmentView?.findViewById<Button>(R.id.account_btn_follow_signout)
-                            ?.background
-                            ?.setColorFilter(ContextCompat.getColor(activity!!, R.color.colorLightGray), PorterDuff.Mode.MULTIPLY)
-//                    fragmentView?.findViewById<Button>(R.id.account_btn_follow_signout)?.background?.BlendModeColorFilter(ContextCompat.getColor(activity!!,R.color.colorLightGray), BlendMode.MULTIPLY)
+                    fragmentView?.findViewById<Button>(R.id.account_btn_follow_signout)?.background?.setColorFilter(ContextCompat.getColor(activity!!,R.color.colorLightGray), Mode.MULTIPLY)
                 }else{
                     if(uid!=currentUserUid){
                         fragmentView?.findViewById<Button>(R.id.account_btn_follow_signout)?.text = getString(R.string.follow)
@@ -108,6 +107,8 @@ class UserFragment:Fragment(){
             }
         }
     }
+
+
 
     fun requestFollow(){
         //Save data to my account
@@ -210,6 +211,76 @@ class UserFragment:Fragment(){
     }
 }
 
-private fun Drawable?.BlendModeColorFilter(color: Int, multiply: BlendMode) {
+fun Drawable.setColorFilter(color: Int, mode: Mode = Mode.SRC_ATOP) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        colorFilter = BlendModeColorFilter(color, mode.getBlendMode())
+    } else {
+        @Suppress("DEPRECATION")
+        setColorFilter(color, mode.getPorterDuffMode())
+    }
+}
+enum class Mode {
+    CLEAR,
+    SRC,
+    DST,
+    SRC_OVER,
+    DST_OVER,
+    SRC_IN,
+    DST_IN,
+    SRC_OUT,
+    DST_OUT,
+    SRC_ATOP,
+    DST_ATOP,
+    XOR,
+    DARKEN,
+    LIGHTEN,
+    MULTIPLY,
+    SCREEN,
+    ADD,
+    OVERLAY;
 
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun getBlendMode(): BlendMode =
+            when (this) {
+                CLEAR -> BlendMode.CLEAR
+                SRC -> BlendMode.SRC
+                DST -> BlendMode.DST
+                SRC_OVER -> BlendMode.SRC_OVER
+                DST_OVER -> BlendMode.DST_OVER
+                SRC_IN -> BlendMode.SRC_IN
+                DST_IN -> BlendMode.DST_IN
+                SRC_OUT -> BlendMode.SRC_OUT
+                DST_OUT -> BlendMode.DST_OUT
+                SRC_ATOP -> BlendMode.SRC_ATOP
+                DST_ATOP -> BlendMode.DST_ATOP
+                XOR -> BlendMode.XOR
+                DARKEN -> BlendMode.DARKEN
+                LIGHTEN -> BlendMode.LIGHTEN
+                MULTIPLY -> BlendMode.MULTIPLY
+                SCREEN -> BlendMode.SCREEN
+                ADD -> BlendMode.PLUS
+                OVERLAY -> BlendMode.OVERLAY
+            }
+
+    fun getPorterDuffMode(): PorterDuff.Mode =
+            when (this) {
+                CLEAR -> PorterDuff.Mode.CLEAR
+                SRC -> PorterDuff.Mode.SRC
+                DST -> PorterDuff.Mode.DST
+                SRC_OVER -> PorterDuff.Mode.SRC_OVER
+                DST_OVER -> PorterDuff.Mode.DST_OVER
+                SRC_IN -> PorterDuff.Mode.SRC_IN
+                DST_IN -> PorterDuff.Mode.DST_IN
+                SRC_OUT -> PorterDuff.Mode.SRC_OUT
+                DST_OUT -> PorterDuff.Mode.DST_OUT
+                SRC_ATOP -> PorterDuff.Mode.SRC_ATOP
+                DST_ATOP -> PorterDuff.Mode.DST_ATOP
+                XOR -> PorterDuff.Mode.XOR
+                DARKEN -> PorterDuff.Mode.DARKEN
+                LIGHTEN -> PorterDuff.Mode.LIGHTEN
+                MULTIPLY -> PorterDuff.Mode.MULTIPLY
+                SCREEN -> PorterDuff.Mode.SCREEN
+                ADD -> PorterDuff.Mode.ADD
+                OVERLAY -> PorterDuff.Mode.OVERLAY
+            }
 }
